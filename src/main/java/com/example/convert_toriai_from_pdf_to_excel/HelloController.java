2fe61d23ee1ce369be736da450f63c44178b2aeb
LeNhaAnticodon <quangnha9210@gmail.com>
2024-06-10 21:02:40 +0900

    hoan thanh giao dien

---
+++ src/main/java/com/example/convert_toriai_from_pdf_to_excel/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.convert_toriai_from_pdf_to_excel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
